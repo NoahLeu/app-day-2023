0d9276309437d1eb673c8d2d855f2bcd3e81972a
NNG0 <96381431+NNG0@users.noreply.github.com>
2023-06-30 21:30:56 +0200

    clean up imports

--- src/components/ActivityCard.tsx
+++ src/components/ActivityCard.tsx
@@ -9,16 +9,7 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from "@/components/ui/select"
-import { Icon } from "@radix-ui/react-select"
 import { FaWheelchair } from "react-icons/fa"
 
 
