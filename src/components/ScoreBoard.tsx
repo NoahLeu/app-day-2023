c2db4b66ee708adc466c687d764690edcd4928e1
Robby <robbykriegk@live.de>
2023-06-30 20:19:15 +0200

    ugly table

--- src/components/ScoreBoard.tsx
+++ src/components/ScoreBoard.tsx
@@ -11,55 +11,28 @@ import {
     TableRow,
   } from "@/components/ui/table"
   
-  const invoices = [
-    {
-      invoice: "INV001",
-      paymentStatus: "Paid",
-      totalAmount: "$250.00",
-      paymentMethod: "Credit Card",
-    },
-    {
-      invoice: "INV002",
-      paymentStatus: "Pending",
-      totalAmount: "$150.00",
-      paymentMethod: "PayPal",
-    },
-    {
-      invoice: "INV003",
-      paymentStatus: "Unpaid",
-      totalAmount: "$350.00",
-      paymentMethod: "Bank Transfer",
-    },
-    {
-      invoice: "INV004",
-      paymentStatus: "Paid",
-      totalAmount: "$450.00",
-      paymentMethod: "Credit Card",
-    },
-    {
-      invoice: "INV005",
-      paymentStatus: "Paid",
-      totalAmount: "$550.00",
-      paymentMethod: "PayPal",
-    },
-    {
-      invoice: "INV006",
-      paymentStatus: "Pending",
-      totalAmount: "$200.00",
-      paymentMethod: "Bank Transfer",
-    },
-    {
-      invoice: "INV007",
-      paymentStatus: "Unpaid",
-      totalAmount: "$300.00",
-      paymentMethod: "Credit Card",
-    },
-  ]
+ 
   
   export function ScoreBoard() {
+   //const [colums, setColums] = React.useState([])
+
+   const colums = [
+    {
+      place: "1",
+      user: "dummy",
+      points: "25",
+    },
+    {
+      place: "2",
+      user: "dummy2",
+      points: "15",
+    },
+  
+  ]
+
     return (
-        <div className="flex flex-col items-center justify-center width 500">
-      <Table>
+        
+      <Table className="w-[350px] text-white">
         <TableCaption>A list of your recent invoices.</TableCaption>
         <TableHeader>
           <TableRow>
@@ -69,17 +42,16 @@ import {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {invoices.map((invoice) => (
-            <TableRow key={invoice.invoice}>
-              <TableCell className="font-medium">{invoice.invoice}</TableCell>
-              <TableCell>{invoice.paymentStatus}</TableCell>
-              <TableCell>{invoice.paymentMethod}</TableCell>
-              <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+          {colums.map((index) => (
+            <TableRow key={index.place}>
+              <TableCell className="font-medium">{index.place}</TableCell>
+              <TableCell>{index.user}</TableCell>
+              <TableCell>{index.points}</TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
-        </div>
+      
     )
   }
   
