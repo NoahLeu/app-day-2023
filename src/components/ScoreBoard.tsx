ea8efdb03fd09f5d7f39f98edec54cb9b005cb93
NNG0 <96381431+NNG0@users.noreply.github.com>
2023-06-30 21:21:28 +0200

    full sized scoreboard

--- src/components/ScoreBoard.tsx
+++ src/components/ScoreBoard.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-
-
 import {
     Table,
     TableBody,
@@ -9,50 +7,50 @@ import {
     TableHead,
     TableHeader,
     TableRow,
-  } from "@/components/ui/table"
-  
- 
-  
-  export function ScoreBoard() {
-   //const [colums, setColums] = React.useState([])
+} from "@/components/ui/table"
+import {Card, CardContent} from "@/components/ui/card";
 
-   const colums = [
-    {
-      place: "1",
-      user: "dummy",
-      points: "25",
-    },
-    {
-      place: "2",
-      user: "dummy2",
-      points: "15",
-    },
-  
-  ]
+export function ScoreBoard() {
+
+    const columns = [
+        {
+            place: "1",
+            user: "dummy",
+            points: "25",
+        },
+        {
+            place: "2",
+            user: "dummy2",
+            points: "15",
+        },
+
+    ]
 
     return (
-        
-      <Table className="w-[350px]">
-        <TableCaption>A list of your recent invoices.</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">Platz</TableHead>
-            <TableHead>User</TableHead>
-            <TableHead>Punkte</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {colums.map((index) => (
-            <TableRow key={index.place}>
-              <TableCell className="font-medium">{index.place}</TableCell>
-              <TableCell>{index.user}</TableCell>
-              <TableCell>{index.points}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      
+        <Card className="m-2 flex-grow">
+            <CardContent>
+                <Table>
+                    <TableCaption>Scoreboard</TableCaption>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead className="w-[100px]">Platz</TableHead>
+                            <TableHead>User</TableHead>
+                            <TableHead>Punkte</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {columns.map((index) => (
+                            <TableRow key={index.place}>
+                                <TableCell className="font-medium">{index.place}</TableCell>
+                                <TableCell>{index.user}</TableCell>
+                                <TableCell>{index.points}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </CardContent>
+        </Card>
     )
-  }
+}
   
 
