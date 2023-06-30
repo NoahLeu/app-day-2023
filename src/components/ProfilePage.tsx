faea392adde2bcbba85b0071206fe11ada748090
Robby <robbykriegk@live.de>
2023-06-30 23:53:16 +0200

    profilePage added

--- src/components/ProfilePage.tsx
+++ src/components/ProfilePage.tsx
@@ -5,15 +5,25 @@ import { Button } from "./ui/button";
 import { Card, CardHeader, CardTitle, CardContent, CardFooter } from "./ui/card";
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
+import { User } from "@prisma/client";
 
-export function ProfilePage() {
+const User = {
+    name: "Max Mustermann",
+    email: "admin@admin.com",
+    location: "Musterstadt",
+    score: 200,
+}
 
-    const User = {
-        name: "Max Mustermann",
-        email: "admin@admin.com",
-        location: "Musterstadt",
-        score: 200,
-    }
+type UserProps = {
+    name: string;
+    email: string;
+    location: string;
+    score: number;
+}
+
+export function ProfilePage({name, email, location, score}: UserProps ) {
+
+
 
     return (
         <Card className="w-[350px]">
@@ -24,18 +34,18 @@ export function ProfilePage() {
                 <form>
                     <div className="grid w-full items-center gap-4">
                         <Label htmlFor="name">Name</Label>
-                        <h4>{User.name}</h4>
+                        <h4>{name}</h4>
                         {/* <Input type="text" id="name" name="name" /> */}
                         <Label htmlFor="email">Email</Label>
-                        <h4>{User.email}</h4>
+                        <h4>{email}</h4>
                         {/* <Input type="email" id="email" name="email" /> */}
                         <Label htmlFor="location">Location</Label>
-                        <h4>{User.location}</h4>
+                        <h4>{location}</h4>
                         {/* <Input type="text" id="location" name="location" /> */}
                         {/* <Label htmlFor="password">Password</Label> */}
                         {/* <Input type="password" id="password" name="password" /> */}
                         <Label htmlFor="challengeScore">Challenge score:</Label>
-                        <h4>{User.score}</h4>
+                        <h4>{score}</h4>
                     </div>
                 </form>
             </CardContent>
